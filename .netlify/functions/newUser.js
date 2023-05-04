f1a287293b5d98251bf61c472b376795a4a10156
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-04 23:15:56 +0200

    Update newUser.js

--- .netlify/functions/newUser.js
+++ .netlify/functions/newUser.js
@@ -1,8 +1,10 @@
-const firebase = require('firebase/app');
-require('firebase/database');
 
-// Initialize Firebase app with your configuration
-firebase.initializeApp({
+
+exports.handler = async (event, context) => {
+
+  try {
+ 
+cont firebase = {
   apiKey: "AIzaSyDDq_cXa6AbPBpsD1Q_d-89Yd7KPTj4kQg",
   authDomain: "api--s.firebaseapp.com",
   databaseURL: "https://api--s-default-rtdb.firebaseio.com",
@@ -11,23 +13,10 @@ firebase.initializeApp({
   messagingSenderId: "242984774494",
   appId: "1:242984774494:web:f0b17eec68ef637d07fdcf",
   measurementId: "G-HPT5J45N07"
-});
-
-exports.handler = async (event, context) => {
-  const { username, fullname, age, password } = event.queryStringParameters;
-
-  try {
-    // Create the main folder with the given name
-    const reff = await firebase.database().ref(username).set('');
-
-    // Create the 'info' subfolder and upload the data
-    const infoRef = firebase.database().ref(`${username}/info`);
-    const fff = await infoRef.set({ username, fullname, age, password });
-
-    // Return success message
+};
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'SUCCESS' })
+      body: JSON.stringify(firebase)
     };
   } catch (error) {
     // Return error message
