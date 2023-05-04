c17218ad8589bf00c6fd07bf943880d1f272ac4d
Abdessattar Elyagoubi <106479409+abdessattar23@users.noreply.github.com>
2023-05-05 00:24:20 +0200

    Update create.js

--- .netlify/functions/create.js
+++ .netlify/functions/create.js
@@ -3,14 +3,20 @@ import { initializeApp } from "firebase/app";
 import { database, ref, set, child } from "firebase/database";
 
 const axios = require("axios");
-const cors = require('cors')({ origin: true });
 
 
 exports.handler = async (event, context) => {
-  cors(event, context, () => {});
   try {
-    const config = await axios.get("https://api-s.netlify.app/.netlify/functions/newUser");
-    firebase.initializeApp(config);
+    firebase.initializeApp({
+apiKey: "AIzaSyDDq_cXa6AbPBpsD1Q_d-89Yd7KPTj4kQg",
+authDomain: "api--s.firebaseapp.com",
+databaseURL: "https://api--s-default-rtdb.firebaseio.com",
+projectId: "api--s",
+storageBucket: "api--s.appspot.com",
+messagingSenderId: "242984774494",
+appId: "1:242984774494:web:f0b17eec68ef637d07fdcf",
+measurementId: "G-HPT5J45N07"
+});
     // Get a reference to the database
 const database = firebase.database();
 
@@ -29,12 +35,18 @@ const database = firebase.database();
     // Return success message
     return {
       statusCode: 200,
+      headers:{
+      'Access-Control-Allow-Origin': '*'
+      },
       body: JSON.stringify({ message: 'SUCCESS' })
     };
   } catch (error) {
     // Return error message
     return {
       statusCode: 500,
+      headers:{
+      'Access-Control-Allow-Origin': '*'
+      },
       body: JSON.stringify({ message: 'FAILED', error })
     };
   }
